f3e4f13aaefed9e8efa1078b1b1bcef8fc86a349
GhostLeader119 <97194393+GhostLeader119@users.noreply.github.com>
2022-01-26 13:05:43 -0700

    Updated main.py, see extended desc.
    -Added try except block for if gamefile is not available
    -Added except block if director class could not be loaded

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-from game.director import Director
+try:
+    from game.director import Director
 
-director = Director()
-director.start_game()
+
+    director = Director()
+    director.start_game()
+except ModuleNotFoundError:
+    #informs user of an error and trys to log it
+    print('\nError: Critical files not found or corrupted...\n')
+    try:
+        from game.error_logger import Error_logger
+        ident = 'ModuleNotFoundError @ main.py'
+        log_master = Error_logger()
+        log_master.scribe(ident)
+    except ModuleNotFoundError:
+        print('Could not log error: File missing')
+except ImportError:
+    #informs user of an error and trys to log it
+    print('\nError: Class or variable could not be retrieved from director.py...\n')
+    try:
+        from game.error_logger import Error_logger
+        ident = 'ImportError @ main.py'
+        log_master = Error_logger()
+        log_master.scribe(ident)
+    except ModuleNotFoundError:
+        print('Could not log error: File missing')
